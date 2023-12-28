083c5d7cdd0618007a6c8066e7487bef9bb8ea19
georgnaver <github@georgnaver.se>
2023-12-28 13:41:44 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,18 +159,7 @@ export default function Page() {
 
       </section>
 
-      <CommandMenu
-        links={[
-          {
-            url: RESUME_DATA.personalWebsiteUrl,
-            title: "Personal Website",
-          },
-          ...RESUME_DATA.contact.social.map((socialMediaLink) => ({
-            url: socialMediaLink.url,
-            title: socialMediaLink.name,
-          })),
-        ]}
-      />
+
     </main>
   );
 }
