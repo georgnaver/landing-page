4006345096e5e024e5a793e218f3e5ef73af29be
georgnaver <github@georgnaver.se>
2023-12-28 09:13:12 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,10 +20,10 @@ import { LinkedInIcon } from "@/components/icons/LinkedInIcon";
 import { XIcon } from "@/components/icons/XIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Georg Navér",
+  initials: "GN",
+  location: "Stockholm, Sweden, CET",
+  locationLink: "https://www.google.com/maps/place/Stockholm",
   about:
     "Full Stack Engineer focused on building products with extra attention to details",
   summary:
